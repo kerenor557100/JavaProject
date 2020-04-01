9748a31f4a7d72db0883a9f452f1ddb6c439ae38
keren or <kerenor557100@gmail.com>
2020-04-01 12:26:03 +0300

    01.04

---
+++ src/UnitTests/RayTest.java
@@ -0,0 +1,28 @@
+package UnitTests;
+
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.Assert.*;
+
+public class RayTest {
+
+    @Test
+    /**
+     *    @author Keren or and avital
+     * Test method for {@link primitives.Ray#Equals(primitives.Ray)}.
+     */
+    public void testEquals() {
+        Vector tmp=new Vector(1,2,3);
+        tmp.normalize();
+        Ray r1= new Ray(new Point3D(1,2,3),(tmp));
+        Ray r2= new Ray(new Point3D(1,2,3),(tmp));
+        Ray r3= new Ray(new Point3D(1,1,3),(tmp));
+
+        assertEquals(r1,r2);
+        assertNotEquals(r1,r3);
+
+    }
+}

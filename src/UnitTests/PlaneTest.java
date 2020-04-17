183b03214818cfdcd20d6058600c3661c1008b01
keren or <kerenor557100@gmail.com>
2020-04-17 15:36:58 +0300

    17.04

--- src/UnitTests/PlaneTest.java
+++ src/UnitTests/PlaneTest.java
@@ -1,8 +1,10 @@
 package UnitTests;
 
+
 import geometries.Plane;
 import org.junit.Test;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 
 import static org.junit.Assert.*;
@@ -33,10 +35,35 @@ public class PlaneTest {
     /**
      * Test method for {@link geometries.Plane#findIntersections(primitives.Ray)}.
      */
+    Plane p1 = new Plane(new Point3D(0, 0, 1), new Vector(0, 1, 0));
+    Plane p2 = new Plane(new Point3D(1, 0, 0), new Vector(1, 0, 1));
+    Ray r1 = new Ray(new Point3D(0, -1, 0), new Vector(0, 1, 0));
+    Ray r2 = new Ray(new Point3D(0, 1, 0), new Vector(0, 1, 0));
+    Ray r3 = new Ray(new Point3D(1, 0, 0), new Vector(1, 0, 0));
     @Test
     public void testFindIntersections() {
 
+        {
+            assertEquals("", p1.findIntersections(r1).size(), 1);
+            //assertEquals("",p1.findIntersections(r1).get(0),new Point3D(0,0,0));
+            try {
+                assertEquals("", p1.findIntersections(r2).size(), 0);
+            }
+            catch (NullPointerException e)
+            {
+                System.out.println("r1 no Intersections p1");
+            }
+            // checks if the vector is contained in the plane there is no intersections
 
+            try {
+                assertEquals("", p2.findIntersections(r3).size(), 0);
+            }
+            catch (NullPointerException e)
+            {
+                System.out.println("r3 no Intersections p2");
+            }
+
+        }
     }
 
 
--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -7,6 +7,8 @@ import primitives.Vector;
 
 import java.util.List;
 
+import static primitives.Util.alignZero;
+
 /**
  *  @author Keren or and avital
  */
@@ -76,7 +78,27 @@ public class Sphere extends RadialGeometry {
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
-        //to do
-        return null;
+        Point3D p0 = ray.getPoint();
+        Vector v = ray.getDirection();
+        Vector u;
+        try {
+            u = _center.subtract(p0);
+        } catch (IllegalArgumentException e) {
+            return List.of(ray.getTargetPoint(_radius));
+        }
+        double tm = alignZero(v.dotProduct(u));
+        double dSquared = tm == 0 ? u.lengthSquared() : u.lengthSquared() - tm * tm;
+        double thSquared = alignZero(_radius * _radius - dSquared);
+        if (thSquared <= 0) return null;
+        double th = alignZero(Math.sqrt(thSquared));
+        if (th == 0) return null;
+        double t1 = alignZero(tm - th);
+        double t2 = alignZero(tm + th);
+        if (t1 <= 0 && t2 <= 0) return null;
+        if (t1 > 0 && t2 > 0) return List.of(ray.getTargetPoint(t1), ray.getTargetPoint(t2));
+        if (t1 > 0)
+            return List.of(ray.getTargetPoint(t1));
+        else
+            return List.of(ray.getTargetPoint(t2));
     }
 }

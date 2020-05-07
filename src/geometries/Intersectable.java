da6e41ea6093e8aec0ba14f418592ceae56b779d
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-07 19:38:17 +0300

    Update Intersectable.java

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -4,13 +4,16 @@ import primitives.*;
 
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Intersectable is a common interface for all geometries that are able
  * to intersect from a ray to their entity (Shape)
  */
 public interface Intersectable {
-
+    /***
+     * GeoPoint
+     */
     public static class GeoPoint {
 
         Geometry _geometry;
@@ -24,6 +27,21 @@ public interface Intersectable {
         public Point3D getPoint() {
             return  new Point3D(point);
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GeoPoint geoPoint = (GeoPoint) o;
+            return Objects.equals(_geometry, geoPoint._geometry) &&
+                    Objects.equals(point, geoPoint.point);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(_geometry, point);
+        }
+
         public Geometry getGeometry() {
             return  _geometry;
         }

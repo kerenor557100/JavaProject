01a0e7a3517a29a1980eaddf9bfe8dc64c7faba4
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-07 19:47:37 +0300

    Update SpotLight.java

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -4,42 +4,26 @@ import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
 
-public class PointLight extends Light implements LightSource {
-    Point3D _position;
-    double _kC;
-    double _kL;
-    double _kQ;
+public class SpotLight extends PointLight{
+    Vector _direction;
 
-    public PointLight(Color colorIntensity, Point3D _position, double _kC, double _kL, double _kQ) {
-        this._intensity = colorIntensity;
-        this._position = new Point3D(_position);
-        this._kC = _kC;
-        this._kL = _kL;
-        this._kQ = _kQ;
-
+    public SpotLight(Color colorIntensity, Point3D _position, Vector _direction, double _kC, double _kL, double _kQ) {
+        super(colorIntensity,_position,_kC,_kL,_kQ);
+        this._direction = new Vector(_direction).normalized();
     }
 
     @Override
     public Color getIntensity(Point3D p) {
-        double dsquared = p.distanceSquared(_position);
-        double d = p.distance(_position);
+        //TODO fix extremes values
+        Color pointLightIntensity = super.getIntensity(p);
+        double projection = _direction.dotProduct(getL(p));
 
-        Color IL = _intensity.reduce(_kC + d*_kL +dsquared* _kQ);
-
+        Color IL = pointLightIntensity.scale(Math.max(0,projection));
         return IL;
     }
 
     @Override
     public Vector getL(Point3D p) {
-        if (p.equals(_position)) {
-            return null;
-        } else {
-            return p.subtract(_position).normalize();
-        }
-//        try {
-//            return p.subtract(_position).normalize();
-//        } catch (IllegalArgumentException e) {
-//            return null;
-
+        return _direction;
     }
 }

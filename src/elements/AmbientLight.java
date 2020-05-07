438c016cc0179cd1de7cf2c045c31d53f38aa41f
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-07 19:43:18 +0300

    Update AmbientLight.java

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -2,23 +2,13 @@ package elements;
 
 import primitives.Color;
 
-public class AmbientLight {
-    private Color _intensity;
-
-
-    public java.awt.Color getIntensity() {
-        return _intensity.getColor();
-    }
-
-
-
-    public void setIntensity(Color _intensity) {
-        this._intensity = _intensity;
-    }
+public class AmbientLight  extends  Light{
+//    public void setIntensity(Color _intensity) {
+//        this._intensity = _intensity;
+//    }
 
     public AmbientLight(Color _intensity, double ka) {
-        // ka is always 1 so we don't use it
-        this._intensity = _intensity;
+        this._intensity = _intensity.scale(ka);
     }
 
 }

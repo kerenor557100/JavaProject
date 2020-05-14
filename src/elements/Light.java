911dfe8b2f6717feb5612f50ae15e158e63cebb6
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-14 13:28:51 +0300

    Update Light.java

--- src/elements/Light.java
+++ src/elements/Light.java
@@ -1,3 +1,4 @@
+
 package elements;
 
 import primitives.Color;
@@ -7,6 +8,10 @@ public abstract class Light {
      * _intensity value, set to protected
      */
     protected Color _intensity;
+
+    public Light(Color _intensity) {
+        this._intensity = _intensity;
+    }
 
     public Color getIntensity() {
         return new Color(_intensity);

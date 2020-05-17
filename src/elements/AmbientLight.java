897584da7bfd9e1eebe8eb0f181bca0e29e202ba
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-17 14:17:30 +0300

    Update AmbientLight.java

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -1,14 +1,14 @@
 package elements;
 
 import primitives.Color;
+//תאורה סביבתית: מקור התאורה הסביבתית מיוצג על ידי מקור אור שהוא קבוע בעוצמתו וקבוע בצבעו, ומשפיע על כל האובייקטים בסצנה באופן שווה.
 
 public class AmbientLight  extends  Light{
 //    public void setIntensity(Color _intensity) {
 //        this._intensity = _intensity;
 //    }
-
+//keren or and avital
     public AmbientLight(Color _intensity, double ka) {
         super(_intensity.scale(ka));
     }
-
 }

2056c52064963fd9b200479dfb24f6775bc5f347
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-21 23:22:46 +0300

    Update Scene.java

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -1,13 +1,17 @@
+
+//מחלקה המאחדת את כל מה שהגדרנו בכל הפרוייקט עד כה כדי ליצור סצנה.
 package scene;
 
-import elements.*;
+import elements.AmbientLight;
+import elements.Camera;
+import elements.LightSource;
 import geometries.Geometries;
 import geometries.Intersectable;
 import primitives.Color;
 
 import java.util.ArrayList;
 import java.util.List;
-//מחלקה המאחדת את כל מה שהגדרנו בכל הפרוייקט עד כה כדי ליצור סצנה.
+
 public class Scene {
     private final String _name;
     private final Geometries _geometries = new Geometries();
@@ -16,12 +20,7 @@ public class Scene {
     private Camera _camera;
     private double _distance;
     private AmbientLight _ambientLight;
-
-
-
     private List<LightSource> _lights = null;
-
-
 
 
     public AmbientLight getAmbientLight() {
@@ -40,27 +39,31 @@ public class Scene {
         return _distance;
     }
 
-
-    public Scene(String _sceneName){
-        this._name= _sceneName;
+    public Scene(String _sceneName) {
+        this._name = _sceneName;
     }
 
     public Color getBackground() {
         return this._background;
     }
+
+
     public List<LightSource> getLightSources() {
         return _lights;
     }
+
     public void addGeometries(Intersectable... intersectables) {
-        for (Intersectable i:intersectables ) {
+        for (Intersectable i : intersectables) {
             _geometries.add(i);
         }
     }
+
     public void removeGeometries(Intersectable... intersectables) {
-        for (Intersectable i:intersectables ) {
+        for (Intersectable i : intersectables) {
             _geometries.remove(i);
         }
     }
+
     public void addLights(LightSource light) {
         if (_lights == null) {
             _lights = new ArrayList<>();
@@ -68,53 +71,70 @@ public class Scene {
         _lights.add(light);
     }
 
+    public void setBackground(Color _background) {
+        this._background = _background;
+    }
 
+    public void setCamera(Camera _camera) {
+        this._camera = _camera;
+    }
 
-    public static class  SceneBuilder  {
-        private  String _name;
-        private Color _background;
-        private  Camera _camera;
-        private  double _distance;
-        private  AmbientLight _ambientLight;
+    public void setDistance(double _distance) {
+        this._distance = _distance;
+    }
 
-        public  SceneBuilder(String _name) {
-            this._name = _name;
+    public void setAmbientLight(AmbientLight _ambientLight) {
+        this._ambientLight = _ambientLight;
+    }
+
+    public void setLights(List<LightSource> _lights) {
+        this._lights = _lights;
+    }
+
+    public static class SceneBuilder {
+        private String name;
+        private Color background;
+        private Camera camera;
+        private double distance;
+        private AmbientLight ambientLight;
+
+        public SceneBuilder(String name) {
+            this.name = name;
         }
 
-        public SceneBuilder addBackground(Color _background) {
-            this._background = _background;
+        public SceneBuilder addBackground(Color background) {
+            this.background = background;
             return this;
         }
 
-        public SceneBuilder addCamera(Camera _camera) {
-            this._camera = _camera;
+        public SceneBuilder addCamera(Camera camera) {
+            this.camera = camera;
             return this;
         }
 
-        public SceneBuilder addDistance(double _distance) {
-            this._distance = _distance;
+        public SceneBuilder addDistance(double distance) {
+            this.distance = distance;
             return this;
         }
 
-        public SceneBuilder addAmbientLight(AmbientLight _ambientLight) {
-            this._ambientLight = _ambientLight;
+        public SceneBuilder addAmbientLight(AmbientLight ambientLight) {
+            this.ambientLight = ambientLight;
             return this;
         }
 
-        public Scene build()
-        {
-            Scene scene = new Scene(_name);
-            scene._camera= _camera;
-            scene._distance = _distance;
-            scene._background = _background;
-            scene._ambientLight =_ambientLight;
+        public Scene build() {
+            Scene scene = new Scene(this.name);
+            scene._camera = this.camera;
+            scene._distance = this.distance;
+            scene._background = this.background;
+            scene._ambientLight = this.ambientLight;
             validateUserObject(scene);
             return scene;
         }
+
         private void validateUserObject(Scene scene) {
             //Do some basic validations to check
             //if user object does not break any assumption of system
         }
     }
-
 }

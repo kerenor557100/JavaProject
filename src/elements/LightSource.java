a86b26b97bb95e22dfed9e461f97f8cb28623d29
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:29:24 +0300

    Update LightSource.java

--- src/elements/LightSource.java
+++ src/elements/LightSource.java
@@ -9,19 +9,14 @@ import primitives.*;
 //מקור תאורה.
 public interface LightSource  {
     /**
-     * Get light source intensity as it reaches a point I<sub>P</sub>
-     *
-     * @param p the lighted point
-     * @return intensity I<sub>P</sub>
+     * Gets light source intensity
+
      */
     Color getIntensity(Point3D p);
 
     /**
      * Get normalized vector in the direction from light source
      * towards the lighted point
-     *
-     * @param p the lighted point
-     * @return light to point vector
      */
     Vector getL(Point3D p);
 

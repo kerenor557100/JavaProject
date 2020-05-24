0a98924b7d0902feadea2bae0783e46cceece4ec
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:32:43 +0300

    Add files via upload

---
+++ src/geometries/FlatGeometry.java
@@ -0,0 +1,24 @@
+package geometries;
+
+import elements.Material;
+import primitives.Color;
+
+/**
+ * all the geometries who did not extends radialgeometry
+ *
+ * we did not decalre it as an interface
+ */
+public abstract class FlatGeometry extends Geometry {
+
+    public FlatGeometry(Color _emission, Material _material) {
+        super(_emission, _material);
+    }
+    public FlatGeometry(Color _emission) {
+        super(_emission);
+    }
+    public FlatGeometry() {
+        super();
+    }
+
+
+}

4190af2839642c1010594f72af30ee421a49165f
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:36:18 +0300

    Update Intersectable.java

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -4,52 +4,44 @@ import primitives.*;
 
 
 import java.util.List;
-import java.util.Objects;
 
 /**
- * Intersectable is a common interface for all geometries that are able
+ * a class for all geometries that are able
  * to intersect from a ray to their entity (Shape)
  */
 public interface Intersectable {
-    /***
-     * GeoPoint
-     */
-    public static class GeoPoint {
 
-        Geometry _geometry;
-        Point3D point;
+    /**
+     * @param ray ray pointing toward a Gepmtry
+     * @return List<GeoPoint> return values
+     */
+    default List<GeoPoint> findIntersections(Ray ray) {
+        return findIntersections(ray, Double.POSITIVE_INFINITY);
+    }
+
+    List<GeoPoint> findIntersections(Ray ray, double maxDistance);
+
+    /**
+     * GeoPoint is just a tuple holding
+     * references to a specific point ain a specific geometry
+     */
+    class GeoPoint {
+
+        protected Geometry _geometry;
+        protected Point3D point;
 
         public GeoPoint(Geometry _geometry, Point3D pt) {
-            this._geometry= _geometry;
-            point =pt;
+            this._geometry = _geometry;
+            point = pt;
         }
 
         public Point3D getPoint() {
-            return  new Point3D(point);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GeoPoint geoPoint = (GeoPoint) o;
-            return Objects.equals(_geometry, geoPoint._geometry) &&
-                    Objects.equals(point, geoPoint.point);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(_geometry, point);
+            return point;
         }
 
         public Geometry getGeometry() {
-            return  _geometry;
+            return _geometry;
         }
-    }  /**
-     *
-     * @param ray ray pointing toward a Gepmtry
-     * @return List<Point3D> return values
-     */
-    List<GeoPoint> findIntersections(Ray ray);
+    }
 
 }

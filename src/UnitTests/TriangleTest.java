ea96750050cca0e4eb3e24b66ef87c9013a98833
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:52:42 +0300

    Update TriangleTest.java

--- src/UnitTests/TriangleTest.java
+++ src/UnitTests/TriangleTest.java
@@ -10,31 +10,10 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for geometries.Triangle class
- * @author elyasaf and omer
+ * @author keren or and avital
  */
 public class TriangleTest {
 
-    /**
-     * Test method for {@link geometries.Triangle#equals(Object)}.
-     */
-    @Test
-    public void testEquals() {
-        // ============ Equivalence Partitions Tests ==============
-
-        Triangle t= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
-        Triangle t2= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
-        assertEquals(t.equals(t2),true);
-    }
-
-
-
-    @Test
-    public void getNormal() {
-        // ============ Equivalence Partitions Tests ==============
-
-        Triangle t= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
-        assertEquals(t.getNormal(),new Vector (new Point3D(-0.5773502691896258, -0.5773502691896258, -0.5773502691896258)));
-    }
 
     Triangle t1=new Triangle(new Point3D(0,0,4),new Point3D(0,0,0),new Point3D(4,0,0));
     Ray r1=new Ray(new Point3D(0,-2,0),new Vector(1,2,1));

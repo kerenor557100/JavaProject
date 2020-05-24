becb986c37f54bac6495a15f25bd0a2b9292c3af
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:34:40 +0300

    Update Geometries.java

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -2,40 +2,35 @@ package geometries;
 
 import primitives.Ray;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Geometries implements Intersectable {
 
-    private List<Intersectable> _geometries = new ArrayList<>();
+    private final List<Intersectable> _geometries = new LinkedList<>();
 
     public Geometries(Intersectable... _geometries) {
         add(_geometries);
     }
 
     public void add(Intersectable... geometries) {
-//        for (Intersectable geo : geometries) {
-//            _geometries.add(geo);
-//        }
         _geometries.addAll(Arrays.asList(geometries));
     }
 
     /**
-     * Na le Hasbir befrotrot
-     *
      * @param ray the ray that intersect the geometries
      * @return list of Point3D that intersect the osef
      */
     @Override
-    public List<GeoPoint> findIntersections(Ray ray) {
+    public List<GeoPoint> findIntersections(Ray ray, double maxDistance) {
         List<GeoPoint> intersections = null;
 
         for (Intersectable geo : _geometries) {
-            List<GeoPoint> tempIntersections = geo.findIntersections(ray);
+            List<GeoPoint> tempIntersections = geo.findIntersections(ray, maxDistance);
             if (tempIntersections != null) {
                 if (intersections == null)
-                    intersections = new ArrayList<>();
+                    intersections = new LinkedList<>();
                 intersections.addAll(tempIntersections);
             }
         }

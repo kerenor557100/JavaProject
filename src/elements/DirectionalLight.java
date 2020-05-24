62b250a2b62abf7d7cf730b60ed451adf9421b49
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:28:07 +0300

    Update DirectionalLight.java

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -1,28 +1,32 @@
-// מקור אור כיווני: (כגון השמש) זהו מקור אור הנמצא באינסוף או במיקום רחוק מאוד, בעל כיוון ואין הנחתה בעקבות המרחק.
 package elements;
 
 import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
-
+// מקור אור כיווני: (כגון השמש) זהו מקור אור הנמצא באינסוף או במיקום רחוק מאוד, בעל כיוון ואין הנחתה בעקבות המרחק.
 public class DirectionalLight extends Light implements LightSource {
-    private Vector _direction;
-
+    private final Vector _direction;
 
     /**
      * Initialize directional light with it's intensity and direction, direction
      * vector will be normalized.
+
      */
 
     public DirectionalLight(Color colorintensity, Vector direction) {
-        super(colorintensity);
+        _intensity = colorintensity;
         _direction = direction.normalized();
     }
 
-  
+    /**
+     * @param p the lighted point is not used and is mentioned
+     *          only for compatibility with LightSource
+     * @return fixed intensity of the directionLight
+     */
     @Override
     public Color getIntensity(Point3D p) {
-        return _intensity;
+        return super.getIntensity();
+        //       return _intensity;
     }
 
     //instead of getDirection()
@@ -30,9 +34,9 @@ public class DirectionalLight extends Light implements LightSource {
     public Vector getL(Point3D p) {
         return _direction;
     }
+
     @Override
     public double getDistance(Point3D point) {
-//        return  this._direction.getHead().distance(point);
         return Double.POSITIVE_INFINITY;
     }
 }

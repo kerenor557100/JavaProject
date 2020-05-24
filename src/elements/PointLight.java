c39ee294754ffd547da8f7e69778b39625d0bfda
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-24 14:30:55 +0300

    Update PointLight.java

--- src/elements/PointLight.java
+++ src/elements/PointLight.java
@@ -1,45 +1,60 @@
+
 package elements;
 
 import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
-//מקור אור נקודתי: מקור אור שאינו כיווני בעל עוצמה.יש מיקום אך לא כיוון.
+
+
+/**
+ *מקור אור נקודתי: מקור אור שאינו כיווני בעל עוצמה.יש מיקום אך לא כיוון.
+ */
 public class PointLight extends Light implements LightSource {
     Point3D _position;
-    double _kC;
-    double _kL;
-    double _kQ;
+    double _kC; // Constant attenuation
+    double _kL; // Linear attenuation
+    double _kQ; // Quadratic attenuation
 
-    public PointLight(Color colorIntensity, Point3D _position, double _kC, double _kL, double _kQ) {
-        super(colorIntensity);
-        this._position = new Point3D(_position);
-        this._kC = _kC;
-        this._kL = _kL;
-        this._kQ = _kQ;
-
+    public PointLight(Color colorIntensity, Point3D position, double kC, double kL, double kQ) {
+        this._intensity = colorIntensity;
+        this._position = new Point3D(position);
+        this._kC = kC;
+        this._kL = kL;
+        this._kQ = kQ;
     }
 
+    // by default, the constant attenuation value is 1 and the other two values are 0
+    public PointLight(Color colorIntensity, Point3D position) {
+        this(colorIntensity, position, 1d, 0d, 0d);
+    }
+
+
+    @Override
+    public Color getIntensity() {
+        return super.getIntensity();
+        //מקבל את העוצמה של האור
+    }
+
+    //overriding LightSource getIntensity(Point3D)
     @Override
     public Color getIntensity(Point3D p) {
         double dsquared = p.distanceSquared(_position);
         double d = p.distance(_position);
 
-        Color IL = _intensity.reduce(_kC + d*_kL +dsquared* _kQ);
-
-        return IL;
+        return (_intensity.reduce(_kC + _kL * d + _kQ * dsquared));
     }
 
+    // Light vector
     @Override
     public Vector getL(Point3D p) {
         if (p.equals(_position)) {
             return null;
-        } else {
-            return p.subtract(_position).normalize();
         }
-//        try {
-//            return p.subtract(_position).normalize();
-//        } catch (IllegalArgumentException e) {
-//            return null;
+        return p.subtract(_position).normalize();
+    }
 
+    @Override
+    public double getDistance(Point3D point) {
+        return _position.distance(point);
     }
 }

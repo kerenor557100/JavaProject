271def305f4d7dcdbbe2cd708e05479d96a5eddd
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-05-25 18:52:17 +0300

    Update Ray.java

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -2,35 +2,45 @@ package primitives;
 
 import static primitives.Util.isZero;
 
+
 public class Ray {
 
+    private static final double DELTA = 0.1;
+
     /**
-     * The point from which the ray is starting.
+     * The point from which the ray starts.
      */
     private final Point3D _point;
     /**
-     * The direction
+     * The direction of the ray.
      */
     private final Vector _direction;
 
     /**
-     * Constructor
+     * Constructor for creating a new instance of this class
+     *
+     * @param point     the start of the ray.
+     * @param direction the direction of the ray.
      */
     public Ray(Point3D point, Vector direction) {
         _point = new Point3D(point);
         _direction = new Vector(direction).normalized();
     }
 
-    /**
-     * @return new Point3D
-     */
-    public Point3D getTargetPoint(double length) {
-        return isZero(length ) ? _point : _point.add(_direction.scale(length));
-    }
+    public Ray(Point3D point, Vector direction, Vector normal) {
+        //head+ normal.scale(±DELTA)
+        _direction = new Vector(direction).normalized();
 
+        double nv = normal.dotProduct(direction);
+
+        Vector normalDelta = normal.scale((nv > 0 ? DELTA : -DELTA));
+        _point = point.add(normalDelta);
+
+    }
 
     /**
      * Copy constructor for a deep copy of an Ray object.
+     *
      * @param other the object that being copied
      */
     public Ray(Ray other) {
@@ -38,17 +48,31 @@ public class Ray {
         this._direction = other._direction.normalized();
     }
 
+    /**
+     * @param length
+     * @return new Point3D
+     */
+    public Point3D getTargetPoint(double length) {
+        Vector targetVector;
+        try {
+            targetVector = _direction.scale(length);
+        } catch (Exception e) {
+            return _point;
+        }
+        return isZero(length) ? _point : _point.add(targetVector);
+    }
+
     @Override
     public boolean equals(Object obj) {
-        if (obj == null){
-            return  false;
+        if (obj == null) {
+            return false;
         }
         if (!(obj instanceof Ray)) {
             return false;
         }
         if (this == obj)
             return true;
-        Ray other = (Ray)obj;
+        Ray other = (Ray) obj;
         return (_point.equals(other._point) &&
                 _direction.equals(other._direction));
     }
@@ -60,6 +84,9 @@ public class Ray {
 
     /**
      * Getter for the point from which the ray starts.
+     *
+     * @return A new Point3D that represents the
+     * point from which the ray starts.
      */
     public Point3D getPoint() {
         return new Point3D(_point);
@@ -67,6 +94,10 @@ public class Ray {
 
     /**
      * Getter for the direction of the ray that is
+     * represented by this object.
+     *
+     * @return A new Vector that represents the
+     * direction of the ray that is
      * represented by this object.
      */
     public Vector getDirection() {

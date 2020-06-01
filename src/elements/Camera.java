5979ed32f7803e26b135e98348b350c143e19424
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-01 17:18:26 +0300

    Update Camera.java

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -4,6 +4,9 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static primitives.Util.isZero;
 //המצלמה!! מרכז המצלמה המשקיף על המודל הוא ראשית הצירים ממנו יוצאים שלושה וקטורים שהם הצירים של המערכת.תפקידה חשוב מאוד במערכת.
 public class Camera {
@@ -14,7 +17,7 @@ public class Camera {
 
     public Camera(Point3D _p0, Vector _vTo, Vector _vUp) {
 
-        //if the the vectors are not orthogonal, throw exception.
+        //must be orthogonal
         if (_vUp.dotProduct(_vTo) != 0)
             throw new IllegalArgumentException("the vectors must be orthogonal");
 
@@ -26,10 +29,7 @@ public class Camera {
 
     }
 
-    /**
-     *
-     *getters
-     */
+//getters
     public Point3D getP0() {
         return new Point3D(_p0);
     }
@@ -67,7 +67,7 @@ public class Camera {
             Pij = Pij.add(_vRight.scale(xj));
         }
         if (!isZero(yi)) {
-            Pij= Pij.add(_vUp.scale(-yi));// Pij.add(_vUp.scale(-yi))
+            Pij = Pij.subtract(_vUp.scale(yi)); 
         }
 
         Vector Vij = Pij.subtract(_p0);
@@ -75,4 +75,64 @@ public class Camera {
         return new Ray(_p0, Vij);
 
     }
+
+    public List<Ray> construct9RaysBeamThroughPixel(int nX, int nY, double i, double j, double screenDist, double screenWidth, double screenHeight) {
+
+        double Rx = screenWidth / nX;//the length of pixel in X axis
+        double Ry = screenHeight / nY;////the length of pixel in Y axis
+
+        double yi = ((i - nY / 2d) * Ry + Ry / 2d);
+        double xj = ((j - nX / 2d) * Rx + Rx / 2d);
+
+
+        Point3D Pc = new Point3D(_p0.add(_vTo.scale(screenDist)));//new point from the camera to the screen
+
+        //pc is the center of the view plane
+        Point3D P = Pc.add(_vRight.scale(xj).subtract(_vUp.scale(yi)));
+
+        //finding the intersection point with the view plane according the formula in the moodle
+
+        //-----SuperSampling-----
+        List<Ray> rays = new LinkedList<>();//the return list, construct Rays Through Pixels
+
+        rays.add(new Ray(P, _p0.subtract(P)));//the first ray(we already find it)
+        //the next 8 rays we gonna add is the same thing, but there's difference in the variation on
+        // x and y arguments, some times we will change one of them and some times both of them
+
+        // x coord middle of pixel/2 downwards
+        Point3D tmp = new Point3D(P.getX().get() - Rx / 2, P.getY().get(), P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // y coord middle of pixel/2 downward
+        tmp = new Point3D(P.getX().get(), P.getY().get() - Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // x coord middle of pixel/2 upwards
+        tmp = new Point3D(P.getX().get() + Rx / 2, P.getY().get(), P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // y coord middle of pixel/2 upward
+        tmp = new Point3D(P.getX().get(), P.getY().get() + Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // x coord middle of pixel/2 downwards  y coord middle of pixel/2 downward
+        tmp = new Point3D(P.getX().get() - Ry / 2, P.getY().get() - Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // x coord middle of pixel/2 upwards  y coord middle of pixel/2 downward
+        tmp = new Point3D(P.getX().get() + Ry / 2, P.getY().get() - Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // x coord middle of pixel/2 downwards    y coord middle of pixel/2 upward
+        tmp = new Point3D(P.getX().get() - Ry / 2, P.getY().get() + Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        // x coord middle of pixel/2 upwards   y coord middle of pixel/2 upward
+        tmp = new Point3D(P.getX().get() + Ry / 2, P.getY().get() + Ry / 2, P.getZ().get());
+        rays.add(new Ray(tmp, new Vector(_p0.subtract(tmp)).normalized()));
+
+        return rays;
+
+    }
+
 }

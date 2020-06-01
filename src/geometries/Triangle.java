93e417036ed475a69858b824a47a563dd6347b0c
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-01 17:10:30 +0300

    Update Triangle.java

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 import static primitives.Util.isZero;
 
+/**
+ * Triangle class
+ */
 public class Triangle extends Polygon {
 
     public Triangle(Color emissionLight, Material material, Point3D p1, Point3D p2, Point3D p3) {
@@ -18,13 +21,20 @@ public class Triangle extends Polygon {
     public Triangle(Color emissionLight, Point3D p1, Point3D p2, Point3D p3) {
         super(emissionLight,p1, p2, p3);
     }
-
+//CTR
     public Triangle(Point3D p1, Point3D p2, Point3D p3) {
         super(p1, p2, p3);
     }
 
 
-
+    @Override
+    public String toString() {
+        String result = "";
+        for (Point3D p : _vertices) {
+            result += p.toString();
+        }
+        return result;
+    }
 
     @Override
     public List<GeoPoint> findIntersections(Ray ray, double maxDistance) {
@@ -38,11 +48,11 @@ public class Triangle extends Polygon {
         Vector v2 = _vertices.get(1).subtract(p0);
         Vector v3 = _vertices.get(2).subtract(p0);
 
-        double s1 = v.dotProduct(v1.crossProduct(v2));
+        double s1 = v.dotProduct(v1.crossProduct(v2).normalized());
         if (isZero(s1)) return null;
-        double s2 = v.dotProduct(v2.crossProduct(v3));
+        double s2 = v.dotProduct(v2.crossProduct(v3).normalized());
         if (isZero(s2)) return null;
-        double s3 = v.dotProduct(v3.crossProduct(v1));
+        double s3 = v.dotProduct(v3.crossProduct(v1).normalized());
         if (isZero(s3)) return null;
 
         if ((s1 > 0 && s2 > 0 && s3 > 0) || (s1 < 0 && s2 < 0 && s3 < 0)) {
@@ -55,13 +65,5 @@ public class Triangle extends Polygon {
 
         return null;
 
-    }
-    @Override
-    public String toString() {
-        String result = "";
-        for (Point3D p : _vertices) {
-            result += p.toString();
-        }
-        return result;
     }
 }

ff2510e53af911df0c6916b9b7d57bd4aa68c288
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-01 17:06:10 +0300

    Update Ray.java

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,11 +1,18 @@
 package primitives;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import static primitives.Util.isZero;
 
-
+/**
+ * Ray class
+ */
 public class Ray {
 
     private static final double DELTA = 0.1;
+    private static final Random rnd = new Random();
 
     /**
      * The point from which the ray starts.
@@ -28,14 +35,13 @@ public class Ray {
     }
 
     public Ray(Point3D point, Vector direction, Vector normal) {
-        //head+ normal.scale(±DELTA)
+        //point + normal.scale(±DELTA)
         _direction = new Vector(direction).normalized();
 
         double nv = normal.dotProduct(direction);
 
         Vector normalDelta = normal.scale((nv > 0 ? DELTA : -DELTA));
         _point = point.add(normalDelta);
-
     }
 
     /**
@@ -45,21 +51,22 @@ public class Ray {
      */
     public Ray(Ray other) {
         this._point = new Point3D(other._point);
-        this._direction = other._direction.normalized();
+        this._direction = other._direction.normalized(); //create new normalized vector
     }
 
     /**
-     * @param length
+     * @param length length to reach the target point
      * @return new Point3D
+     * @author Dan Zilberstein
      */
     public Point3D getTargetPoint(double length) {
-        Vector targetVector;
-        try {
-            targetVector = _direction.scale(length);
-        } catch (Exception e) {
+        if (isZero(length)) {
             return _point;
         }
-        return isZero(length) ? _point : _point.add(targetVector);
+
+        Vector targetVector = _direction.scale(length);
+
+        return _point.add(targetVector);
     }
 
     @Override
@@ -70,11 +77,12 @@ public class Ray {
         if (!(obj instanceof Ray)) {
             return false;
         }
-        if (this == obj)
+        if (this == obj) {
             return true;
+        }
+
         Ray other = (Ray) obj;
-        return (_point.equals(other._point) &&
-                _direction.equals(other._direction));
+        return (_point.equals(other._point) && _direction.equals(other._direction));
     }
 
     @Override
@@ -83,8 +91,6 @@ public class Ray {
     }
 
     /**
-     * Getter for the point from which the ray starts.
-     *
      * @return A new Point3D that represents the
      * point from which the ray starts.
      */
@@ -93,14 +99,40 @@ public class Ray {
     }
 
     /**
-     * Getter for the direction of the ray that is
-     * represented by this object.
-     *
      * @return A new Vector that represents the
      * direction of the ray that is
      * represented by this object.
      */
     public Vector getDirection() {
-        return new Vector(_direction);
+        return _direction.normalized();
     }
+
+    public List<Ray> constructRayBeamThroughPixel(Ray originalRay, Point3D targetPoint, double delta, int amount) {
+       
+        if (isZero(originalRay.getPoint().distance(targetPoint))) {
+            throw new IllegalArgumentException("distance cannot be 0");
+        }
+        Vector direction = originalRay.getDirection();
+        Point3D point = direction.getHead();
+
+        Vector n1 = new Vector(point.getY()._coord * -1d, point.getX()._coord, 0d);
+        Vector n2 = new Vector(point.getY()._coord, point.getX()._coord * -1d, 0d);
+        double x;
+        double y;
+
+
+        LinkedList<Ray> rays = new LinkedList<>();
+
+        double[] doubles = rnd.doubles(amount * 2, -1 * delta, delta).distinct().toArray();
+        for (int i = 0; i < doubles.length; i++) {
+            Point3D Pxy = targetPoint;
+            x = doubles[i];
+            y = doubles[++i];
+            Pxy = Pxy.add(n1.scale(x)).add(n2.scale(y));
+            rays.add(new Ray(point, point.subtract(point)));
+        }
+        return rays;
+
+    }
+
 }

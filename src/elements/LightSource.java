ea29e1ada0e9615e9b2a7491b536ebf44ce55d23
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-11 14:42:39 +0300

    Update LightSource.java

--- src/elements/LightSource.java
+++ src/elements/LightSource.java
@@ -1,6 +1,10 @@
 package elements;
 
-import primitives.*;
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Vector;
+
+import java.util.List;
 
 /**
  * Interface for common actions of light sources
@@ -13,13 +17,22 @@ public interface LightSource  {
 
      */
     Color getIntensity(Point3D p);
-
+    double getRadius();
+    Point3D getPosition();
     /**
      * Get normalized vector in the direction from light source
      * towards the lighted point
+     *
+     * @param p the lighted point
+     * @return light to point vector
      */
-    Vector getL(Point3D p);
 
+    Vector getL(Point3D p);
+    List<Vector> getLs (Point3D p);
+    /**
+     * @param point from geopoint
+     * @return distance from light source
+     */
     double getDistance(Point3D point);
 
 

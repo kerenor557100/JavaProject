b3a42ccff8c9918861fb4e745f5228270a6d00db
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-11 14:45:53 +0300

    Update FlatGeometry.java

--- src/geometries/FlatGeometry.java
+++ src/geometries/FlatGeometry.java
@@ -4,26 +4,19 @@ import elements.Material;
 import primitives.Color;
 
 /**
+ * Flat Geometry is a Marker abstract class extending Geometry
+ * to differentiate it from RadialGeometry
  * we did not decalre it as an interface
- * geometries without radius
  */
 public abstract class FlatGeometry extends Geometry {
-    /**
-     * Associated plane in which the flat geometry lays
-     */
-    protected Plane _plane;
 
     public FlatGeometry(Color _emission, Material _material) {
         super(_emission, _material);
     }
-
     public FlatGeometry(Color _emission) {
         super(_emission);
     }
-
     public FlatGeometry() {
         super();
     }
-
-    public abstract long getNormal();
 }

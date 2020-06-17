2ad18b63b6fd2ab986f2f581f0eb735e671bc1db
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-17 12:52:38 +0300

    Update ProjectTests.java

--- src/UnitTests/ProjectTests.java
+++ src/UnitTests/ProjectTests.java
@@ -222,4 +222,63 @@ public void Soft_Shadows3spheres() {
         render.writeToImage();
     }
 
+
+    @Test
+    public void SoftShadows_wow() {
+        Scene scene = new Scene("Test scene");
+        scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+        scene.setDistance(1000);
+        scene.setBackground(Color.BLACK);
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+
+        scene.addGeometries( //
+                new Triangle((Color.BLACK), new Material(0.5, 0.5, 20,1, 0.5), //
+                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
+                new Triangle((Color.BLACK), new Material(0.5, 0.5, 20,1, 0.5), //
+                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 140), new Point3D(75, -75, 150)), //
+                //גוף
+                new Sphere(new Color(200,240,0), new Material(0.2, 0.2, 30, 0.6, 0), 2.4083, new Point3D(0, 0, 0)),
+                new Sphere(new Color(200,240,0), new Material(0.2, 0.2, 30, 0.6, 0), 1.5905, new Point3D(0, -4, 0)),
+                //גוף
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), 0.6, new Point3D(1.79, -5.25, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), 0.6082, new Point3D(-1.82, -5.19, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), 0.8485, new Point3D(2.23, 2.32, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), 0.8360, new Point3D(-2.2, 2.31, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), .5261, new Point3D(3.48, 2.64, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), .5, new Point3D(-4.01, -1.39, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), 0.5477, new Point3D(-3.39, 2.94, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), .8306, new Point3D(2.74, -1.71, 0)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), .8306, new Point3D(-2.77, -1.69, 0)),
+                //עיניים
+                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), .5, new Point3D(-.77, -4.47, 1.31)),
+                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), .5, new Point3D(.67, -4.47, 1.31)),
+                new Sphere(new Color(java.awt.Color.YELLOW), new Material(0.2, 0.2, 30, 0.6, 0), .5, new Point3D(4.04, -1.55, 0)),
+
+
+
+                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        3, new Point3D(-12, -2, 8)),
+
+
+                new Sphere(new Color(java.awt.Color.GREEN), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        3, new Point3D(-2, 12, 8)),
+
+                new Sphere(new Color(java.awt.Color.PINK), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        3, new Point3D(12, 2, 8)));
+//radius!!!!
+        scene.addLights(new PointLight(new Color(700, 400, 400), //
+                new Point3D(90, -80, 50 ), 1, 4E-5, 2E-7,4));
+        scene.addLights(new DirectionalLight(new Color(500, 300, 0), new Vector(1, -1, 1)));
+        scene.addLights(new SpotLight(new Color(1020, 400, 400),  new Point3D(-750, 750, 150),
+                new Vector(-1, 1, 4), 1, 0.00001, 0.000005));
+
+        ImageWriter imageWriter = new ImageWriter("BIRTHDAY!!", 50, 50, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+
+
+        render.renderImage();
+        render.writeToImage();
+    }
+
 }

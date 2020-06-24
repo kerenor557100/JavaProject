9b7a984b84cfaba352246fccf0149a66fe681073
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-24 13:04:56 +0300

    Update TriangleTest.java

--- src/UnitTests/TriangleTest.java
+++ src/UnitTests/TriangleTest.java
@@ -6,7 +6,7 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Unit tests for geometries.Triangle class
@@ -14,6 +14,29 @@ import static org.junit.Assert.assertEquals;
  */
 public class TriangleTest {
 
+    /**
+     * Test method for {@link geometries.Triangle#equals(Object)}.
+     */
+    @Test
+    public void testEquals() {
+        // ============ Equivalence Partitions Tests ==============
+
+        Triangle t= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
+        Triangle t2= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
+        assertEquals(t.equals(t2),true);
+    }
+
+
+    /**
+     * Test method for {@link geometries.Triangle#getNormal()}.
+     */
+    @Test
+    public void getNormal() {
+        // ============ Equivalence Partitions Tests ==============
+
+        Triangle t= new Triangle(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,0,1));
+        assertEquals(t.getNormal(),new Vector (new Point3D(-0.5773502691896258, -0.5773502691896258, -0.5773502691896258)));
+    }
 
     Triangle t1=new Triangle(new Point3D(0,0,4),new Point3D(0,0,0),new Point3D(4,0,0));
     Ray r1=new Ray(new Point3D(0,-2,0),new Vector(1,2,1));
@@ -27,42 +50,42 @@ public class TriangleTest {
     @Test
     public void findIntersectionsTests() {
 
-        assertEquals("",t1.findIntersections(r1).size(),1);
+        assertEquals("",t1.getFindIntersections(r1).size(),1);
 
         try {
-            t1.findIntersections(r2).size();
+            t1.getFindIntersections(r2).size();
         }
         catch (NullPointerException e){
             System.out.println(e+ "r2 no Intersections with t1");
         }
 
         try {
-            t1.findIntersections(r3).size();
+            t1.getFindIntersections(r3).size();
         }
         catch (NullPointerException e){
             System.out.println("r3 no Intersections with t1");
         }
 
         try {
-            t1.findIntersections(r4).size();
+            t1.getFindIntersections(r4).size();
         }
         catch (NullPointerException e){
             System.out.println("r4 no Intersections with t1");
         }
         try{
-            t2.findIntersections(r1).size();
+            t2.getFindIntersections(r1).size();
         }
         catch (NullPointerException e){
             System.out.println("r1 no Intersections with t2");
         }
         try{
-            t2.findIntersections(r2).size();
+            t2.getFindIntersections(r2).size();
         }
         catch (NullPointerException e){
             System.out.println("r2 no Intersections with t2");
         }
         try{
-            t2.findIntersections(r3).size();
+            t2.getFindIntersections(r3).size();
         }
         catch (NullPointerException e){
             System.out.println("r3 no Intersections with t2");
@@ -70,7 +93,7 @@ public class TriangleTest {
 
         try{
 
-            assertEquals("",t2.findIntersections(r4).size(),1);
+            assertEquals("",t2.getFindIntersections(r4).size(),1);
 
         }
         catch (NullPointerException e){

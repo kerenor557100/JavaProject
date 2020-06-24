ac4b298acf95c1e905597e681a4c324d078ce3a6
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-06-24 13:01:26 +0300

    Update Triangle.java

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -15,6 +15,8 @@ public class Triangle extends Polygon {
 
     public Triangle(Color emissionLight, Material material, Point3D p1, Point3D p2, Point3D p3) {
         super(emissionLight,material,p1,p2,p3);
+        buildBox();
+
     }
     public Triangle(Color emissionLight, Point3D p1, Point3D p2, Point3D p3) {
         super(emissionLight,p1, p2, p3);
@@ -27,7 +29,48 @@ public class Triangle extends Polygon {
         return super.getNormal(super._plane._p);
     }
 
+    private void buildBox()
+    {
+        Box b=new Box();
+        double minX = Double.MAX_VALUE;
+        double minY = Double.MAX_VALUE;
+        double minZ = Double.MAX_VALUE;
+        double maxX = Double.MIN_VALUE;
+        double maxY = Double.MIN_VALUE;
+        double maxZ = Double.MIN_VALUE;
 
+        for (Point3D p :  this._vertices)
+        {
+
+            if (p.getX().get() < minX) {
+                minX = p.getX().get();
+            }
+            if (p.getY().get() < minY) {
+                minY = p.getY().get();
+            }
+            if (p.getZ().get() < minZ) {
+                minZ = p.getZ().get();
+            }
+            if (p.getX().get() > maxX) {
+                maxX = p.getX().get();
+            }
+            if (p.getY().get() > maxY) {
+                maxY = p.getY().get();
+            }
+            if (p.getZ().get() > maxZ) {
+                maxZ = p.getZ().get();
+            }
+        }
+        // Point3D center = new Point3D((maxX + minX) / 2, (maxY + minY) / 2, (maxZ + minZ) / 2);
+        this.box._max_X=maxX;
+        this.box._max_Y=maxY;
+        this.box._max_Z=maxZ;
+        this.box._min_X=minX;
+        this.box._min_Y=minY;
+        this.box._min_Z=minZ;
+
+
+    }
     @Override
     public String toString() {
         String result = "";
@@ -38,8 +81,8 @@ public class Triangle extends Polygon {
     }
 
     @Override
-    public List<GeoPoint> findIntersections(Ray ray) {
-        List<GeoPoint> planeIntersections = _plane.findIntersections(ray);
+    public List<GeoPoint> getFindIntersections(Ray ray, double max) {
+        List<GeoPoint> planeIntersections = _plane.findIntersections(ray,max);
         if (planeIntersections == null) return null;
 
         Point3D p0 = ray.getPoint();
@@ -60,7 +103,7 @@ public class Triangle extends Polygon {
             //for GeoPoint
             List<GeoPoint> result = new LinkedList<>();
             for (GeoPoint geo : planeIntersections) {
-                result.add(new GeoPoint(this, geo.getPoint()));
+                result.add(new GeoPoint(this, geo._point));
             }
             return result;
         }

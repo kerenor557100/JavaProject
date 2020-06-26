7fd82c06c9afef9d33725f3546eeee467f7637b7
Avital99 <57639012+Avital99@users.noreply.github.com>
2020-06-26 10:34:07 +0300

    Update AmbientLight.java

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -3,11 +3,19 @@ package elements;
 import primitives.Color;
 //תאורה סביבתית: מקור התאורה הסביבתית מיוצג על ידי מקור אור שהוא קבוע בעוצמתו וקבוע בצבעו, ומשפיע על כל האובייקטים בסצנה באופן שווה.
 
+//keren or and avital projet
+
 public class AmbientLight  extends  Light{
-//    public void setIntensity(Color _intensity) {
-//        this._intensity = _intensity;
-//    }
-//keren or and avital
+    
+/**    public void setIntensity(Color _intensity) {
+*       this._intensity = _intensity;
+*   }
+*/
+    
+    
+ /** אמביאנט לייט הוא שילוב של שלושת הפרמטרים של הצבע המוכפלים בעוצמה
+ */
+    
 public AmbientLight(Color ia, double ka) {
     this._intensity = ia.scale(ka);
 }
